c99996479cd9654fc4d0938606c4754073a977c9
Simon van den Berg <simonschrotenberg@gmail.com>
2023-04-21 14:29:53 +0200

    added the functionality to get orders from the database

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //test code, can be removed
+        Database.open();
+        Order order1 = new Order(1);
+        System.out.println(order1);
+        Database.close();
     }
 }
---
+++ src/Order.java
@@ -0,0 +1,102 @@
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Order {
+    private int orderNumber;
+    // arrayList of products
+    private Customer customer;
+    private ArrayList<Product> products = new ArrayList<Product>();
+
+    public Order(int orderNumber) {
+        this.orderNumber = orderNumber;
+        getProductsFromDatabase();
+        getCustomerFromDatabase();
+    }
+
+    @Override
+    public String toString() {
+        return "Order [orderNumber= " + orderNumber + ",\n customer= " + customer + ",\n products= " + products + "]";
+    }
+
+    private void getCustomerFromDatabase() {
+        try {
+            // Create a statement
+            Statement statement = Database.connection.createStatement();
+
+            // Execute a statement
+            ResultSet result = statement
+                    .executeQuery("SELECT * FROM orders where order_number = " + this.orderNumber + ";");
+
+            // Loop over the result set
+            while (result.next()) {
+                this.customer = new Customer(result.getInt("customer_id"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed to execute the query.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * this method will get all the products related to the order.
+     * 
+     * @return
+     */
+    private void getProductsFromDatabase() {
+        try {
+            // Create a statement
+            Statement statement = Database.connection.createStatement();
+
+            // Execute a statement
+            ResultSet result = statement
+                    .executeQuery("SELECT * FROM order_lines where order_number = " + this.orderNumber + ";");
+
+            // Loop over the result set
+            while (result.next()) {
+                products.add(getProductFromDatabase(result.getInt("product_id"), result.getInt("amount")));
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed to execute the query.");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * this method will get a single product from the database.
+     * 
+     * @param id     the id of the product
+     * @param amount the ordered amount of the product
+     * @return Product
+     */
+    private Product getProductFromDatabase(int id, int amount) {
+        Product product = null;
+        try {
+            // Create a statement
+            Statement statement = Database.connection.createStatement();
+
+            // Execute a statement
+            ResultSet result = statement
+                    .executeQuery("SELECT * FROM products where id = " + id + ";");
+
+            // Loop over the result set
+            while (result.next()) {
+                product = new Product(result.getInt("id"), result.getString("name"), result.getString("color"), amount);
+            }
+        } catch (SQLException e) {
+            System.err.println("Failed to execute the query.");
+            e.printStackTrace();
+        }
+        return product;
+    }
+
+    /**
+     * this method will get a single product from the database.
+     * it also defaults the amount to 1.
+     * 
+     * @param id
+     * @return Product
+     */
+    private Product getProductFromDatabase(int id) {
+        return getProductFromDatabase(id, 1);
+    }
+}
