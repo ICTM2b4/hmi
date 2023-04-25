d76b378183c4b50800cf681ce0608fdc9bb4589b
Simon van den Berg <58862159+Simon6837@users.noreply.github.com>
2023-04-25 16:01:58 +0200

    added the functionality to get orders from the database (#8)

---
+++ src/Product.java
@@ -0,0 +1,22 @@
+public class Product {
+    private int id;
+    private String name;
+    private String color;
+    private int amount;
+
+    public Product(int id, String name, String color, int amount) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id= " + id + ", name= " + name + ", color= " + color + ", amount= " + amount + "]";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
