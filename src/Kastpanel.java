b08002a63671d3f5b9d381f273f7de6a061b57f7
kvincn <115541436+kvincn@users.noreply.github.com>
2023-04-28 10:02:27 +0200

    kast gevuld met blokjes

--- src/Kastpanel.java
+++ src/Kastpanel.java
@@ -6,6 +6,9 @@ public Kastpanel(){
     setBackground(Color.white);
 
 }
+ private double map(long x, long in_min, long in_max, double out_max, long out_min){
+    return ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
@@ -18,6 +21,17 @@ public Kastpanel(){
         for (int i = 0; i < 6; i++) {
             g2.drawLine(((((getWidth() -70)/11)*(i*2))+35),50,((((getWidth() -70)/11)*(i*2))+35),((((getHeight()-100)/5)*5)+50));
         }
+        g2.setStroke(new BasicStroke(2));
+        for (int h = 0; h < 5; h++) {
+            for (int i = 0; i < 5; i++) {
+                g2.setColor(Color.red);
+                g2.fillRect(Integer.valueOf((int) ((47 * map(getWidth(), 492, 960, 1.30, 1)) + (i * (map(getWidth(), 492, 960, 2.15, 1) * 75)))), Integer.valueOf((int) ((60 * map(getHeight(), 435, 983, 1.30, 1)) + (h * (map(getHeight(), 435, 983, 2.6, 1) * 67.5)))), (getWidth() / 44) * 5, (getHeight() / 40) *5);
+                System.out.println(Integer.valueOf((int) ((50.628 * map(getWidth(), 492, 960, 1.30, 1)) + (i * (map(getWidth(), 492, 960, 2.15, 1) * 75)))));
+                g2.setColor(Color.black);
+                g2.drawRect(Integer.valueOf((int) ((47 * map(getWidth(), 492, 960, 1.30, 1)) + (i * (map(getWidth(), 492, 960, 2.15, 1) * 75)))), Integer.valueOf((int) ((60 * map(getHeight(), 435, 983, 1.30, 1)) + (h * (map(getHeight(), 435, 983, 2.6, 1) * 67.5)))), (getWidth() / 44) * 5, (getHeight() / 40) *5);
+            }
+        }
+
 
     }
 }
