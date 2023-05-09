e26996b6034ae2607c43b999892c1bebd49efa43
Tygozwolle <95019431+Tygozwolle@users.noreply.github.com>
2023-05-09 13:25:27 +0200

    pakbon

--- src/GeneratePDF.java
+++ src/GeneratePDF.java
@@ -7,6 +7,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class GeneratePDF {
 
@@ -26,24 +27,69 @@ public static void generatePDF() {
         //Open the document.
         // Add content to the document.
         document.open();
-        int[] columnWidths = {1, 5, 5};
-        document.add(new Paragraph("Ordernummer: " + Order.selectOrder.getOrderNumber()));
-         document.add(new Paragraph("Naam: " + Order.selectOrder.getCustomer().getName()));
-            document.add(new Paragraph("Adres: " + Order.selectOrder.getCustomer().getAddress()));
-            document.add(new Paragraph("E-mail: " + Order.selectOrder.getCustomer().getEmail()));
-            document.add(new Paragraph("Telefoon: " + Order.selectOrder.getCustomer().getPhoneNumber()));
-        document.add(new Paragraph(" "));
+        PdfPTable pdfPTable2 = new PdfPTable(new float[] { 14,30 }) ;
+        //Create cells
+        PdfPCell pdfPCellcatogory = new PdfPCell(new Paragraph("Ordernummer:"));
+        PdfPCell pdfPCelldata = new PdfPCell(new Paragraph(" "+Order.selectOrder.getOrderNumber()));
+        pdfPCelldata.setBorderWidth(0);
+        pdfPCellcatogory.setBorderWidth(0);
+        pdfPTable2.addCell(pdfPCellcatogory);
+        pdfPTable2.addCell(pdfPCelldata);
+
+        pdfPCellcatogory = new PdfPCell(new Paragraph("Naam:"));
+        pdfPCelldata = new PdfPCell(new Paragraph(" "+Order.selectOrder.getCustomer().getName()));
+        pdfPCelldata.setBorderWidth(0);
+        pdfPCellcatogory.setBorderWidth(0);
+        pdfPTable2.addCell(pdfPCellcatogory);
+        pdfPTable2.addCell(pdfPCelldata);
+
+        pdfPCellcatogory = new PdfPCell(new Paragraph("Adres:"));
+        pdfPCelldata = new PdfPCell(new Paragraph(" "+Order.selectOrder.getCustomer().getAddress()));
+        pdfPCelldata.setBorderWidth(0);
+        pdfPCellcatogory.setBorderWidth(0);
+        pdfPTable2.addCell(pdfPCellcatogory);
+        pdfPTable2.addCell(pdfPCelldata);
+
+        pdfPCellcatogory = new PdfPCell(new Paragraph("E-mail:"));
+        pdfPCelldata = new PdfPCell(new Paragraph(" "+Order.selectOrder.getCustomer().getEmail()));
+        pdfPCelldata.setBorderWidth(0);
+        pdfPCellcatogory.setBorderWidth(0);
+        pdfPTable2.addCell(pdfPCellcatogory);
+        pdfPTable2.addCell(pdfPCelldata);
+
+        pdfPCellcatogory = new PdfPCell(new Paragraph("Telefoon:"));
+        pdfPCelldata = new PdfPCell(new Paragraph(" "+Order.selectOrder.getCustomer().getPhoneNumber()));
+        pdfPCelldata.setBorderWidth(0);
+        pdfPCellcatogory.setBorderWidth(0);
+        pdfPTable2.addCell(pdfPCellcatogory);
+        pdfPTable2.addCell(pdfPCelldata);
+
+
+        pdfPTable2.setWidthPercentage(50);
+        pdfPTable2.setHorizontalAlignment(0);
+
+
+        document.add(pdfPTable2);
+
+//        document.add(new Paragraph("Ordernummer: " + Order.selectOrder.getOrderNumber()));
+//         document.add(new Paragraph("Naam: " + Order.selectOrder.getCustomer().getName()));
+//            document.add(new Paragraph("Adres: " + Order.selectOrder.getCustomer().getAddress()));
+//            document.add(new Paragraph("E-mail: " + Order.selectOrder.getCustomer().getEmail()));
+//            document.add(new Paragraph("Telefoon: " + Order.selectOrder.getCustomer().getPhoneNumber()));
+//        document.add(new Paragraph(" "));
         PdfPTable pdfPTable = new PdfPTable(new float[] { 75,25 }) ;
         //Create cells
         PdfPCell pdfPCell1 = new PdfPCell(new Paragraph("Productnaam"));
         PdfPCell pdfPCell2 = new PdfPCell(new Paragraph("Aantal"));
-
-
         //Add cells to table
         pdfPTable.addCell(pdfPCell1);
         pdfPTable.addCell(pdfPCell2);
-
-
+        ArrayList<Product> products = Order.selectOrder.getProducts();
+        for(Product product : products){
+            pdfPTable.addCell(product.getName());
+            pdfPTable.addCell(String.valueOf(product.getAmount()));
+        }
+   document.add(new Paragraph(" "));
         document.add(pdfPTable);
 
 
