331378e7d403c7639d3ad1c599416f1959c0e9b6
kvincn <115541436+kvincn@users.noreply.github.com>
2023-05-17 10:15:06 +0200

    Revert "ordernummer in WijzigDialog krijgen"
    This reverts commit 134a66deacc8b5b4589da91d1c159e4e381cb1ab.

--- src/OrderInfoPanel.java
+++ src/OrderInfoPanel.java
@@ -103,14 +103,6 @@ public class OrderInfoPanel extends JFrame implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            WijzigDialog dialog = new WijzigDialog(this);
-        } catch(java.lang.NullPointerException a){
-            jlAddress.setText("Geef een Ordernummer op");
-            jlCustomerAddress.setText("Geef een Ordernummer op");
-            jlCustomerEmail.setText("Geef een Ordernummer op");
-            jlCustomerName.setText("Geef een Ordernummer op");
-            jlCustomerPhoneNumber.setText("Geef een Ordernummer op");
-        }
+        WijzigDialog dialog = new WijzigDialog(this);
     }
 }
