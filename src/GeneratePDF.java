380b5bb4a05f02aa100cf576a8d62357ee88d04f
Sam Gijsbers <sam.gijsbers14@gmail.com>
2023-05-30 10:16:56 +0200

    Added several boxes

--- src/GeneratePDF.java
+++ src/GeneratePDF.java
@@ -74,20 +74,75 @@ public class GeneratePDF {
                 document.add(pdfPTable2);
 
 
-                PdfPTable pdfPTable = new PdfPTable(new float[] { 75, 25 });
+                PdfPTable pdfPTableProd = new PdfPTable(new float[] { 75, 25 });
+                PdfPTable pdfPTableProd2 = new PdfPTable(new float[] { 75, 25 });
+                PdfPTable pdfPTableProd3 = new PdfPTable(new float[] { 75, 25 });
+
                 // Create cells
                 PdfPCell pdfPCell1 = new PdfPCell(new Paragraph("Productnaam"));
                 PdfPCell pdfPCell2 = new PdfPCell(new Paragraph("Aantal"));
                 // Add cells to table
-                pdfPTable.addCell(pdfPCell1);
-                pdfPTable.addCell(pdfPCell2);
+                pdfPTableProd.addCell(pdfPCell1);
+                pdfPTableProd.addCell(pdfPCell2);
+
+                pdfPTableProd2.addCell(pdfPCell1);
+                pdfPTableProd2.addCell(pdfPCell2);
+
+                pdfPTableProd3.addCell(pdfPCell1);
+                pdfPTableProd3.addCell(pdfPCell2);
+
+                Boolean box1 = false;
+                Boolean box2 = false;
+                Boolean box3 = false;
+                int total = 0;
+                int counter = 0;
+
                 ArrayList<Product> products = Order.selectOrder.getProducts();
                 for (Product product : products) {
-                    pdfPTable.addCell(product.getName());
-                    pdfPTable.addCell(String.valueOf(product.getAmount()));
+                    total = total + product.getAmount();
+                    
+                    //Fill boxes
+                    for(int i = 0; i < product.getAmount(); i++){
+                        if(!box1){
+                            pdfPTableProd.addCell(product.getName());
+                            pdfPTableProd.addCell("1");
+                            counter++;
+                            if(counter == 3){
+                                box1 = true;
+                                counter = 0;
+                            }
+                        }else if(!box2){
+                            pdfPTableProd2.addCell(product.getName());
+                            pdfPTableProd2.addCell("1");
+                            counter++;
+                            if(counter == 3){
+                                box2 = true;
+                                counter = 0;
+                            }
+                        }else if(!box3){
+                            pdfPTableProd3.addCell(product.getName());
+                            pdfPTableProd3.addCell("1");
+                            counter++;
+                            if(counter == 3){
+                                box3 = true;
+                                counter = 0;
+                            }
+                        }
+                    }
                 }
                 document.add(new Paragraph(" "));
-                document.add(pdfPTable);
+                document.add(new Paragraph("Doos 1"));
+                document.add(pdfPTableProd);
+
+                if(total >= 3){
+                document.add(new Paragraph("Doos 2"));
+                document.add(pdfPTableProd2);
+                }
+
+                if(total >= 6){
+                document.add(new Paragraph("Doos 3"));
+                document.add(pdfPTableProd3);
+                }
 
                 // Close document and outputStream.
                 document.close();
